6bd62f7cbcbe2d36c4299459e60a05a696d9d8a1
paka_coding <wnstjd5408@naver.com>
2022-01-14 17:45:28 +0900

    reverse_lazy를 써서 login.html로 이동

--- Django-board/board/common/views.py
+++ Django-board/board/common/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import *
 # Create your views here.
-from .forms import UserCreationForm
+from .forms import *
+from django.urls import reverse_lazy
 
 
 class UserCreateView(CreateView):
     form_class = UserCreationForm
     template_name = 'common/join.html'
-    success_url = "/"
+    success_url = reverse_lazy('common:login')
+
+
+class UserUpdateView(UpdateView):
+    form_class = UserChangeForm
+    template_name = 'common/update.html'
