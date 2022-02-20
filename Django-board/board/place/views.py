e7e9497905580d9c46ab0ef772dc24127310a4d7
paka_coding <wnstjd5408@naver.com>
2022-02-20 23:03:59 +0900

    추천시스템 기능 추가

--- Django-board/board/place/views.py
+++ Django-board/board/place/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import *
 from .models import Place, Location
+from django.db.models import Max
 # Create your views here.
 from django.views.generic.list import MultipleObjectMixin
+import random
 
 
 class LocationListView(ListView):
@@ -12,6 +14,18 @@ class LocationListView(ListView):
 
     def get_queryset(self):
         return Location.objects.order_by('id')
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        max_id = Place.objects.all().aggregate(max_id=Max('id'))['max_id']
+        while True:
+            pk = random.randint(1, max_id)
+            place = Place.objects.filter(pk=pk).first()
+            if place:
+                context['random'] = place
+                break
+        return context
 
 
 class LocationDetailView(MultipleObjectMixin, DetailView):
@@ -27,7 +41,14 @@ class LocationDetailView(MultipleObjectMixin, DetailView):
         object_list = Place.objects.filter(location=self.get_object())
 
         context = super().get_context_data(object_list=object_list, ** kwargs)
-
+        max_id = Place.objects.all().aggregate(max_id=Max('id'))['max_id']
+        while True:
+            pk = random.randint(1, max_id)
+            place = Place.objects.filter(
+                pk=pk, location=self.get_object()).first()
+            if place:
+                context['random'] = place
+                break
         context['location'] = Location.objects.filter(
             id=self.kwargs['pk'])
         return context
